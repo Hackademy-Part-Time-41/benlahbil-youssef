5a8958f10851b325615fbbecdf96845e4e3a8a98
Youssef Benlahbil <youssefben86@live.it>
2024-07-09 23:45:57 +0200

    Esercizio1

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Numeri pari da 1 a 20');
+     
+    for(let i = 1; i <= 20; i++){
+
+    if(i % 2 == 0){
+
+        console.log(i);
+    }
+         }
+ console.log('Fine');
+     
+        
